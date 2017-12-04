8fc7acebfb9340efb0a85fb0e89cdbaca534eadd
ivan123king <lw18751836671@163.com>
2017-12-04 17:52:17 +0800

    java生成二维码

---
+++ Exercise/src/com/lw/qrcode/GenerateQRCode.java
@@ -0,0 +1,29 @@
+package com.lw.qrcode;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.sun.imageio.plugins.common.ImageUtil;
+
+public class GenerateQRCode {
+	
+}
